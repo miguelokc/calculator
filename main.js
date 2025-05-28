af9ec7543736c989a51499a6a2cb70c0c2028eb1
miguelokc <185961450+miguelokc@users.noreply.github.com>
2025-05-27 20:45:23 -0500

    more button functionality

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 function callOne(num){
-    document.getElementsByClassName("screen")[0].textContent = num;
+    const screen = document.getElementsByClassName("screen")[0];
+    screen.textContent += num;
+}
+function addition(){
+    const screen = document.getElementsByClassName("screen")[0];
+    screen.textContent += "+";
+}
+function subtraction(){
+    const screen = document.getElementsByClassName("screen")[0];
+    screen.textContent += "-";
+}
+function multiply(){
+    const screen = document.getElementsByClassName("screen")[0];
+    screen.textContent += "*";
+}
+function divide(){
+    const screen = document.getElementsByClassName("screen")[0];
+    screen.textContent += "/";
+}
+function erase(){
+    const screen = document.getElementsByClassName("screen")[0];
+    screen.textContent = "";
 }
