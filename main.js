8e41d4a526d7c628b71c2d7d73832a3de4f0c0b4
miguelokc <185961450+miguelokc@users.noreply.github.com>
2025-05-28 14:40:40 -0500

    added logic to calculator

--- main.js
+++ main.js
@@ -1,24 +1,70 @@
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+let isSecond = false;
+
 function callOne(num){
     const screen = document.getElementsByClassName("screen")[0];
-    screen.textContent += num;
+    if(!isSecond){
+        firstNumber += num;
+        screen.textContent = firstNumber;
+    } else{
+        secondNumber += num;
+        screen.textContent = firstNumber + operator + secondNumber;
+    }
+    
 }
-function addition(){
+function setOperator(op){
     const screen = document.getElementsByClassName("screen")[0];
-    screen.textContent += "+";
+   if (firstNumber == "") return;
+    operator = op;
+    isSecond = true;
+    screen.textContent = firstNumber + operator;
 }
-function subtraction(){
+
+function equals(){
     const screen = document.getElementsByClassName("screen")[0];
-    screen.textContent += "-";
+
+    const num1 = parseFloat (firstNumber);
+    const num2 = parseFloat (secondNumber);
+
+    const result = operate(operator, num1 , num2);
+
+    screen.textContent = result;
+
 }
-function multiply(){
-    const screen = document.getElementsByClassName("screen")[0];
-    screen.textContent += "*";
-}
-function divide(){
-    const screen = document.getElementsByClassName("screen")[0];
-    screen.textContent += "/";
-}
+
 function erase(){
+
+     firstNumber = "";
+     secondNumber = "";
+     operator = "";
+     isSecond = false;
+
     const screen = document.getElementsByClassName("screen")[0];
     screen.textContent = "";
+
+}
+
+function addition(a,b){
+    return a + b;
+}
+function subtraction(a,b){
+   return a - b;
+}
+function multiply(a,b){
+    return a * b;
+}
+function divide(a,b){
+    return a / b;
+}
+
+function operate(operator, a , b){
+    switch(operator){
+        case "+": return addition(a,b);
+        case "-": return subtraction(a,b);
+        case "*": return multiply(a,b);
+        case "/": return divide(a,b);
+        default: return "invalid";
+    }
 }
